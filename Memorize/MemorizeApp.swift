41da8547a717e8edd54fede7d072a399ede1bc67
Aekam <106927980+aekams@users.noreply.github.com>
2023-05-06 11:17:14 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aekam on 2023-05-06.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
